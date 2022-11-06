f3fc411b9aba8d7260b3f5012fc32b749fc59411
effie <havealittlefaith2096@gmail.com>
2022-11-06 02:00:15 +0000

    Added dirs for treesitter queries + overrides for java folds.

---
+++ queries/java/folds.scm
@@ -0,0 +1,12 @@
+[
+(class_declaration
+  name: (identifier))
+(record_declaration
+  name: (identifier))
+(enum_declaration
+  name: (identifier))
+(method_declaration
+  name: (identifier))
+(constructor_declaration
+  name: (identifier))
+] @fold
