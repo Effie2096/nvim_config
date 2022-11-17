65ee3854e0bae1ec27f5a93052db20da45c7cda6
effie <havealittlefaith2096@gmail.com>
2022-11-17 06:29:13 +0000

    fix(plugin): Visual tweaks for registers.nvim conf.
    Picked "nicer" white space symbols.
    Fixed barely visible highlight group for named registers.

--- after/plugin/registers.lua
+++ after/plugin/registers.lua
@@ -3,4 +3,38 @@ if not status_ok then
 	return
 end
 
-registers.setup()
+registers.setup({
+	symbols = {
+		-- Show a special character for line breaks
+		newline = "﬋",
+		-- Show space characters without changes
+		space = "•",
+		-- Show a special character for tabs
+		tab = "▸▸",
+		-- The character to show when a register will be applied in a char-wise fashion
+		register_type_charwise = "ᶜ",
+		-- The character to show when a register will be applied in a line-wise fashion
+		register_type_linewise = "ˡ",
+		-- The character to show when a register will be applied in a block-wise fashion
+		register_type_blockwise = "ᵇ",
+	},
+	window = {
+		transparency = 0
+	},
+	 -- Highlight the sign registers as regular Neovim highlights
+	sign_highlights = {
+		cursorline = "Visual",
+		selection = "Constant",
+		default = "Function",
+		unnamed = "Statement",
+		read_only = "Type",
+		expression = "Exception",
+		black_hole = "Error",
+		alternate_buffer = "Operator",
+		last_search = "Tag",
+		delete = "Special",
+		yank = "Delimiter",
+		history = "Number",
+		named = "String",
+	}
+})
