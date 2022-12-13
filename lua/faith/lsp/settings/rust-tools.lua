668c291a2976d7b3dca8b658b7b46c07c128c730
effie <havealittlefaith2096@gmail.com>
2022-12-13 05:34:46 +0000

    refactor: use mason installed rust debugger

--- lua/faith/lsp/settings/rust-tools.lua
+++ lua/faith/lsp/settings/rust-tools.lua
@@ -1,3 +1,6 @@
+local extension_path = vim.env.XDG_DATA_HOME .. '/nvim/mason/packages/codelldb/extension/'
+local codelldb_path = extension_path .. 'adapter/codelldb'
+local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
 return {
 	tools = {
 		executor = require("rust-tools.executors").termopen,
@@ -10,5 +13,9 @@ return {
 		hover_actions = {
 			auto_focus = true,
 		},
-	}
+	},
+	dap = {
+		adapter = require('rust-tools.dap').get_codelldb_adapter(
+			codelldb_path, liblldb_path)
+	},
 }
