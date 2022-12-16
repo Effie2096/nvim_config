aad38ae879acabab71a635dd5b2f57b38cd0ba4c
effie <havealittlefaith2096@gmail.com>
2022-12-16 02:09:26 +0000

    fix: listchars toggle
    Toggle indentblankline as well so tabs are visible.
    Set win local listchars instead of global.
    Added keymap for toggle.

--- lua/faith/listchars.lua
+++ lua/faith/listchars.lua
@@ -1,6 +1,7 @@
 vim.opt.list = true
 
 local icons = require('faith.icons')
+local nnoremap = require('faith.keymap').nnoremap
 
 local empty_chars = "eol: ,extends: ,nbsp: ,precedes: ,space: ,tab:  ,trail:" .. icons.characters.trail
 local listchars =
@@ -14,13 +15,17 @@ local listchars =
 
 local cycle_list = function ()
 	---@diagnostic disable-next-line: undefined-field
-	if vim.opt.listchars._value == empty_chars then
-		vim.opt.listchars = listchars
-		vim.cmd('IndentBlanklineDisable')
+	if vim.opt_local.listchars._value == empty_chars or vim.opt_local.listchars._value == "" then
+		vim.opt_local.listchars = listchars
+		if package.loaded.indent_blankline ~= nil then
+			vim.cmd('IndentBlanklineDisable')
+		end
 		vim.notify("List characters set.")
 	else
-		vim.opt.listchars = empty_chars
-		vim.cmd('IndentBlanklineEnable')
+		vim.opt_local.listchars = empty_chars
+		if package.loaded.indent_blankline ~= nil then
+			vim.cmd('IndentBlanklineEnable')
+		end
 		vim.notify("List characters cleared.")
 	end
 end
@@ -28,3 +33,4 @@ end
 vim.opt.listchars = empty_chars
 
 vim.api.nvim_create_user_command("ToggleListChars", cycle_list, {})
+nnoremap("<leader>lc", "<cmd>ToggleListChars<CR>", { noremap = true, silent = true })
