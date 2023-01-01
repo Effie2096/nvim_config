5f158974a2039f506a373f89578c04a22ae27561
Effie2096 <havealittlefaith2096@gmail.com>
2023-01-01 01:57:16 +0000

    refactor(config): turn off lsp auto install

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -4,6 +4,14 @@ if not status_ok then
 end
 
 toggleterm.setup({
+	size = function (term)
+		if term.direction == "horizontal" then
+			return 16
+		elseif term.direction == "vertical" then
+			local term_win_width = math.floor(vim.o.columns * 0.4)
+			return term_win_width > 81 and term_win_width or 81
+		end
+	end,
 	open_mapping = [[<c-\>]],
 	insert_mappings = true,
 	terminal_mappings = true,
