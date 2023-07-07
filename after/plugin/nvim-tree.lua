816251b0cdffbc4fe59688d649504d077d7991a1
Effie2096 <havealittlefaith2096@gmail.com>
2023-07-07 19:45:30 +0100

    feat: nvim tree hotkey changes and lsp plugin

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -16,7 +16,65 @@ nnoremap("<leader>ef", "<cmd>NvimTreeToggle<CR>", desc(opts, "[e]dit [f]iles: to
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local api = require("nvim-tree.api")
+
+local function edit_or_open()
+	local node = api.tree.get_node_under_cursor()
+
+	if node.nodes ~= nil then
+		-- expand or collapse folder
+		api.node.open.edit()
+	else
+		-- open file
+		api.node.open.edit()
+		-- Close the tree if file was opened
+		api.tree.close()
+	end
+end
+
+-- open as vsplit on current node
+local function split_preview(direction)
+	local node = api.tree.get_node_under_cursor()
+
+	if node.nodes ~= nil then
+		-- expand or collapse folder
+		api.node.open.edit()
+	else
+		if direction == "h" then
+			-- open file in split
+			api.node.open.horizontal()
+		elseif direction == "v" then
+			-- open file as vsplit
+			api.node.open.vertical()
+		end
+	end
+
+	-- Finally refocus on tree if it was lost
+	api.tree.focus()
+end
+
+local function tree_on_attach(bufnr)
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	api.config.mappings.default_on_attach(bufnr)
+
+	vim.keymap.set("n", "<CR>", api.node.open.tab_drop, opts("Tab drop"))
+
+	vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
+	vim.keymap.set("n", "J", function()
+		split_preview("h")
+	end, opts("Split Preview"))
+	vim.keymap.set("n", "L", function()
+		split_preview("v")
+	end, opts("Vsplit Preview"))
+	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close"))
+	vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+end
+
 nvim_tree.setup({
+	on_attach = tree_on_attach,
 	renderer = {
 		group_empty = true,
 		indent_markers = {
@@ -25,15 +83,16 @@ nvim_tree.setup({
 		root_folder_label = false,
 	},
 	view = {
-		width = 49,
+		width = 30,
 	},
 	filters = {
+		custom = { "^.git$" },
 		dotfiles = false,
 	},
 	diagnostics = {
 		enable = true,
 		show_on_dirs = true,
-		show_on_open_dirs = true,
+		show_on_open_dirs = false,
 		debounce_delay = 50,
 		severity = {
 			min = vim.diagnostic.severity.HINT,
@@ -48,6 +107,10 @@ nvim_tree.setup({
 	},
 })
 
+local has_lsp_file_operations, lsp_file_operations = pcall(require, "lsp-file-operations")
+if has_lsp_file_operations then
+	lsp_file_operations.setup()
+end
 --[[ local function open_nvim_tree()
 	require("nvim-tree.api").tree.open()
 end
