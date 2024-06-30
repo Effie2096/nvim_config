3b5cfd558abb74d7a3e5daafcc7005b342dec516
Effie2096 <havealittlefaith2096@gmail.com>
2024-06-30 16:33:26 +0100

    feat: gitsigns key binds

--- after/plugin/nvim-gitsigns-config.lua
+++ after/plugin/nvim-gitsigns-config.lua
@@ -85,8 +85,14 @@ gitsign.setup({
 		end, { expr = true })
 
 		-- Actions
-		map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-		map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+		map("n", "<leader>hs", gs.stage_hunk)
+		map("n", "<leader>hr", gs.reset_hunk)
+		map("v", "<leader>hs", function()
+			gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
+		map("v", "<leader>hr", function()
+			gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
 		-- map('n', '<leader>hS', gs.stage_buffer)
 		map("n", "<leader>hu", gs.undo_stage_hunk)
 		-- map('n', '<leader>hR', gs.reset_buffer)
@@ -98,6 +104,7 @@ gitsign.setup({
 			gs.diffthis("~")
 		end)
 		-- map('n', '<leader>td', gs.toggle_deleted)
+		map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 	end,
 })
 
