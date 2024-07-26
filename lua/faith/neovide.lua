2cd29a0e426db087952bc0b418984e2c29253901
Effie2096 <havealittlefaith2096@gmail.com>
2024-07-26 20:47:13 +0100

    feat: neovide font resize keymaps

--- lua/faith/neovide.lua
+++ lua/faith/neovide.lua
@@ -17,3 +17,20 @@ vim.keymap.set({ "n" }, "<F11>", function()
 		vim.g.neovide_fullscreen = false
 	end
 end, { silent = true })
+
+local resize = function(increase)
+	local font = vim.o.guifont
+	local font_size_flag = font:match(":h%d+")
+	local font_size = tonumber(font_size_flag:sub(3, -1))
+	font_size = increase and (font_size + 1) or (font_size - 1)
+	local font_string = font:gsub(font_size_flag, string.format(":h%d", font_size))
+	vim.notify("Font size: " .. font_size)
+	vim.cmd([[set guifont=]] .. font_string)
+end
+vim.keymap.set({ "n", "i" }, "<C-->", function()
+	resize(false)
+end, { silent = true })
+
+vim.keymap.set({ "n", "i" }, "<C-=>", function()
+	resize(true)
+end, { silent = true })
