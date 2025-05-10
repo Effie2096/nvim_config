4db1fc5e31d8eea1191fac4451aeca4de7e2774b
Effie2096 <havealittlefaith2096@gmail.com>
2025-05-10 14:52:22 +0100

    fix: set ts-context height based on vim height

--- lua/faith/plugins/treesitter.lua
+++ lua/faith/plugins/treesitter.lua
@@ -7,40 +7,53 @@ return {
 		dependencies = {
 			{
 				"romgrk/nvim-treesitter-context",
-				opts = {
-					enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-					multiwindow = true,
-					throttle = true, -- Throttles plugin updates (may improve performance)
-					max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-					mode = "topline",
-					patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-						-- For all filetypes
-						-- Note that setting an entry here replaces all other patterns for this entry.
-						-- By setting the 'default' entry below, you can control which nodes you want to
-						-- appear in the context window.
-						default = {
-							"class",
-							"function",
-							"method",
-							"for", -- These won't appear in the context
-							"while",
-							"if",
-							"switch",
-							"case",
+				opts = function()
+					local context_height = vim.o.lines / 4
+					local opts = {
+						enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+						multiwindow = true,
+						throttle = true, -- Throttles plugin updates (may improve performance)
+						max_lines = context_height, -- How many lines the window should span. Values <= 0 mean no limit.
+						mode = "topline",
+						patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+							-- For all filetypes
+							-- Note that setting an entry here replaces all other patterns for this entry.
+							-- By setting the 'default' entry below, you can control which nodes you want to
+							-- appear in the context window.
+							default = {
+								"class",
+								"function",
+								"method",
+								"for",
+								"while",
+								"if",
+								"switch",
+								"case",
+							},
+							-- Example for a specific filetype.
+							-- If a pattern is missing, *open a PR* so everyone can benefit.
+							--	 rust = {
+							--		 'impl_item',
+							--	 },
 						},
-						-- Example for a specific filetype.
-						-- If a pattern is missing, *open a PR* so everyone can benefit.
-						--	 rust = {
-						--		 'impl_item',
-						--	 },
-					},
-					exact_patterns = {
-						-- Example for a specific filetype with Lua patterns
-						-- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-						-- exactly match "impl_item" only)
-						-- rust = true,
-					},
-				},
+						exact_patterns = {
+							-- Example for a specific filetype with Lua patterns
+							-- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
+							-- exactly match "impl_item" only)
+							-- rust = true,
+						},
+					}
+					return opts
+				end,
+				init = function()
+					vim.api.nvim_create_autocmd("VimResized", {
+						callback = function()
+							require("treesitter-context").setup({
+								max_lines = math.floor(vim.o.lines / 4),
+							})
+						end,
+					})
+				end,
 			},
 			"nvim-treesitter/nvim-treesitter-textobjects",
 			{
