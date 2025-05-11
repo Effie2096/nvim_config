dce0f130c98738d52c548deb82c912180bdea970
Effie2096 <havealittlefaith2096@gmail.com>
2025-05-12 00:59:22 +0100

    feat(plugin): add neo-tree

--- lua/faith/plugins/nav.lua
+++ lua/faith/plugins/nav.lua
@@ -112,4 +112,62 @@ return {
 			},
 		},
 	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+			{
+				"antosha417/nvim-lsp-file-operations",
+				dependencies = {
+					"nvim-lua/plenary.nvim",
+				},
+				config = function()
+					require("lsp-file-operations").setup()
+				end,
+			},
+			{
+				"s1n7ax/nvim-window-picker", -- for open_with_window_picker keymaps
+				version = "2.*",
+				config = function()
+					require("window-picker").setup({
+						filter_rules = {
+							include_current_win = false,
+							autoselect_one = true,
+							-- filter using buffer options
+							bo = {
+								-- if the file type is one of following, the window will be ignored
+								filetype = {
+									"neo-tree",
+									"neo-tree-popup",
+									"notify",
+									"OverseerList",
+									"undotree",
+									"Outline",
+								},
+								-- if the buffer type is one of following, the window will be ignored
+								buftype = { "terminal", "quickfix" },
+							},
+						},
+					})
+				end,
+			},
+		},
+		lazy = false, -- neo-tree will lazily load itself
+		---@module "neo-tree"
+		---@type neotree.Config?
+		opts = {
+			popup_border_style = "winborder",
+		},
+		keys = {
+			{
+				"<leader>ef",
+				"<cmd>Neotree filesystem reveal left<cr>",
+				desc = "[e]xplore [f]iles: Open file explorer.",
+			},
+		},
+	},
 }
