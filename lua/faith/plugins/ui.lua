0856db90da12024b717f39fa10edd0a6bd705da1
Effie2096 <havealittlefaith2096@gmail.com>
2025-05-20 01:09:18 +0100

    feat(plugin): yazi and snacks

--- lua/faith/plugins/ui.lua
+++ lua/faith/plugins/ui.lua
@@ -2,6 +2,29 @@ local icons = require("faith.icons")
 
 return {
 	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			bigfile = { enabled = true },
+			dashboard = { enabled = false },
+			explorer = { enabled = true },
+			indent = { enabled = false },
+			input = { enabled = true },
+			picker = { enabled = true },
+			notifier = { enabled = false },
+			quickfile = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = false },
+			statuscolumn = { enabled = false },
+			words = { enabled = true },
+		},
+	},
+	{
 		"lukas-reineke/indent-blankline.nvim",
 		name = "ibl",
 		config = function()
