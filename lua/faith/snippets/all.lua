8bac94e652ea5c54bbd41333232be0d829e18b4c
Effie2096 <havealittlefaith2096@gmail.com>
2025-05-22 19:29:47 +0100

    feat(snippets): add timestamp snippets

--- lua/faith/snippets/all.lua
+++ lua/faith/snippets/all.lua
@@ -1,16 +1,36 @@
---[[ local ls = require "luasnip"
-if not pcall(require, "luasnip") then
-  return
-end ]]
+local has_luasnip, ls = pcall(require, "luasnip")
+if not has_luasnip then
+	return
+end
 
---[[ local s = ls.s
+local s = ls.s
 local i = ls.insert_node
 local t = ls.text_node
 local f = ls.function_node
 local c = ls.choice_node
 
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local rep = require("luasnip.extras").rep ]]
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local fmta = require("luasnip.extras.fmt").fmta
+-- local rep = require("luasnip.extras").rep
 
-return {}
+local timestamp = function()
+	return os.date("%Y-%m-%dT%H:%M:%S.000")
+end
+
+local rfc = function()
+	return os.date("%a, %d %b %Y %H:%M:%S")
+end
+
+-- TODO: calculate BST based on date ig lol <20-05-25>
+return {
+	s("timestamp", {
+		f(timestamp, {}),
+		c(1, { t("+01:00"), t("Z") }),
+		i(0),
+	}),
+	s("rfc822", {
+		f(rfc, {}),
+		c(1, { t(" +0100"), t(" GMT") }),
+		i(0),
+	}),
+}
