da031d06d6cbf099ffef833cbc507906ffc1adda
Effie2096 <havealittlefaith2096@gmail.com>
2025-05-29 15:23:05 +0100

    refactor(term): make toggleterm responsive + use bash

--- lua/faith/plugins/runner.lua
+++ lua/faith/plugins/runner.lua
@@ -67,28 +67,63 @@ return {
 	},
 	{
 		"akinsho/toggleterm.nvim",
-		opts = {
-			size = function(term)
-				if term.direction == "horizontal" then
-					return 16
-				elseif term.direction == "vertical" then
+		opts = {},
+		init = function()
+			local term_opts = function()
+				local direction = vim.o.columns > 180 and "vertical"
+					or "horizontal"
+				local size = 16
+				if direction == "horizontal" then
+					local term_win_height = math.floor(vim.o.lines * 0.2)
+					size = term_win_height > 120 and term_win_height or 10
+				elseif direction == "vertical" then
 					local term_win_width = math.floor(vim.o.columns * 0.4)
-					return term_win_width > 81 and term_win_width or 81
+					size = term_win_width > 81 and term_win_width or 81
 				end
-			end,
-			open_mapping = [[<c-t>]],
-			insert_mappings = true,
-			terminal_mappings = true,
-		},
-		keys = {
-			{
-				"<c-t>",
-				function()
-					require("toggleterm").toggle({ direction = "horizontal" })
+				return { size = size, direction = direction }
+			end
+			vim.keymap.set("n", "<c-t>", function()
+				local opts = term_opts()
+				vim.api.nvim_cmd({
+					cmd = "ToggleTerm",
+					args = {
+						"size=" .. opts.size,
+						"direction=" .. opts.direction,
+					},
+					count = 1,
+				}, { output = false })
+			end)
+			vim.keymap.set("i", "<c-t>", function()
+				local opts = term_opts()
+				vim.api.nvim_cmd({
+					cmd = "ToggleTerm",
+					args = {
+						"size=" .. opts.size,
+						"direction=" .. opts.direction,
+					},
+					count = 1,
+				}, { output = false })
+			end)
+
+			vim.api.nvim_create_autocmd("TermEnter", {
+				pattern = "term://*toggleterm#*",
+				callback = function(args)
+					vim.schedule(function()
+						vim.cmd("startinsert")
+					end)
+					vim.keymap.set("t", "<c-t>", function()
+						vim.api.nvim_cmd({
+							cmd = "ToggleTerm",
+							args = {
+								"size=16",
+								"direction=horizontal",
+							},
+							count = 1,
+						}, {})
+					end, { buffer = args.buf })
 				end,
-				{ "n", "i" },
-			},
-		},
+			})
+		end,
 	},
 	{
 		"willothy/flatten.nvim",
