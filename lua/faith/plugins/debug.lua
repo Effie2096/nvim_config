65b2928e318e552393eef972e8b3380de9055627
effie <havealittlefaith2096@gmail.com>
2025-06-10 15:26:58 +0100

    fix: mason registry api change

--- lua/faith/plugins/debug.lua
+++ lua/faith/plugins/debug.lua
@@ -179,10 +179,12 @@ return {
 					command = "node",
 					-- 💀 Make sure to update this path to point to your installation
 					args = {
-						require("mason-registry")
-							.get_package("js-debug-adapter")
-							:get_install_path()
-							.. "/js-debug/src/dapDebugServer.js",
+						vim.fn.globpath(
+							"$MASON/share/js-debug-adapter",
+							"/js-debug/src/dapDebugServer.js",
+							true,
+							true
+						),
 						"${port}",
 					},
 				},
