302c95f64c1a2d0e5789c227fe0e238ac4b57854
effie <havealittlefaith2096@gmail.com>
2025-07-30 01:14:57 +0100

    feat(colorschemes): yorumi

--- lua/faith/plugins/colorschemes.lua
+++ lua/faith/plugins/colorschemes.lua
@@ -57,6 +57,13 @@ local themes = {
 				vim.o.background = "dark"
 			]],
 		},
+		{
+			name = "Yorumi Abyss",
+			colorscheme = "yorumi",
+			before = [[
+				vim.o.background = "dark"
+			]],
+		},
 	},
 
 	-- Light
@@ -674,6 +681,108 @@ local theme_colormaps = {
 
 		return color_map
 	end,
+	["yorumi"] = function(scheme)
+		local palette = {
+			dark = {
+
+				-- yoru
+				yoru0 = "#060914",
+				yoru1 = "#0C0F1A",
+				yoru2 = "#121520",
+				yoru3 = "#1D202B",
+
+				-- tsuki
+				tsuki0 = "#656771",
+				tsuki1 = "#878996",
+				tsuki2 = "#A7A9B5",
+				tsuki3 = "#BDBFCB",
+				tsuki4 = "#C6DFEC",
+
+				-- kuroi
+				kuroiRed = "#4E0E0E",
+				kuroiGreen = "#1C4642",
+				kuroiBlue = "#0D2C4E",
+				kuroiYellow = "#6B5905",
+				kuroiViolet = "#0E0D17",
+				kuroiBlack = "#121210",
+				kuroiGray = "#343742",
+
+				-- umi
+				umiRed = "#913B3B",
+				umiOrange = "#9C672B",
+				umiGreen = "#667C4B",
+				umiBlue = "#42778A",
+				umiYellow = "#9D672F",
+				umiMagenta = "#8D3F5A",
+				umiCyan = "#49837E",
+
+				-- sango
+				sangoRed = "#F05C60",
+				sangoOrange = "#D29146",
+				sangoGreen = "#80AA6E",
+				sangoBlue = "#597BC0",
+				sangoYellow = "#BA9A5E",
+				sangoMagenta = "#B4647F",
+				sangoViolet = "#A188C3",
+				sangoCyan = "#7AA8A7",
+
+				-- kairo
+				kairoRed = "#F47171",
+				kairoOrange = "#F3AB59",
+				kairoGreen = "#9CB67D",
+				kairoBlue = "#788AD3",
+				kairoYellow = "#D6B476",
+				kairoMagenta = "#DA72A2",
+				kairoViolet = "#958EBE",
+				kairoCyan = "#85C7B8",
+			},
+		}
+
+		---@type ColorMap
+		local color_map = {
+			bg = palette[scheme].yoru0,
+			bg_light = palette[scheme].yoru2,
+			bg_dark = palette[scheme].yoru1,
+			fg = palette[scheme].tsuki3,
+			fg_light = palette[scheme].tsuki4,
+			fg_dark = palette[scheme].tsuki2,
+			float = palette[scheme].kuroiBlack,
+			float_light = palette[scheme].kuroiGray,
+			float_dark = palette[scheme].kuroiViolet,
+			surface = palette[scheme].yoru3,
+			surface_light = palette[scheme].kuroiGray,
+			surface_dark = palette[scheme].yoru2,
+			accent = palette[scheme].kairoMagenta,
+			red = palette[scheme].umiRed,
+			red_light = palette[scheme].kairoRed,
+			red_dark = palette[scheme].kuroiRed,
+			green = palette[scheme].umiGreen,
+			green_light = palette[scheme].sangoGreen,
+			green_dark = palette[scheme].kuroiGreen,
+			blue = palette[scheme].sangoBlue,
+			blue_light = palette[scheme].kairoBlue,
+			blue_dark = palette[scheme].kuroiBlue,
+			cyan = palette[scheme].sangoCyan,
+			cyan_light = palette[scheme].kairoCyan,
+			cyan_dark = palette[scheme].umiCyan,
+			pink = palette[scheme].sangoMagenta,
+			pink_light = palette[scheme].kairoMagenta,
+			pink_dark = palette[scheme].umiMagenta,
+			purple = palette[scheme].sangoViolet,
+			purple_light = palette[scheme].kairoViolet,
+			purple_dark = palette[scheme].kuroiViolet,
+			yellow = palette[scheme].sangoYellow,
+			yellow_light = palette[scheme].kairoYellow,
+			yellow_dark = palette[scheme].kuroiYellow,
+			orange = palette[scheme].sangoOrange,
+			orange_light = palette[scheme].kairoOrange,
+			orange_dark = palette[scheme].umiOrange,
+			error = palette[scheme].umiRed,
+			warn = palette[scheme].umiOrange,
+			info = palette[scheme].umiBlue,
+			hint = palette[scheme].umiCyan,
+		}
+
 		return color_map
 	end,
 }
@@ -1722,6 +1831,17 @@ return {
 				pattern = { "sakura" },
 				callback = function()
 					apply_theme_overrides("sakura", vim.o.background)
+				end,
+			})
+		end,
+	},
+	{
+		"yorumicolors/yorumi.nvim",
+		init = function()
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				pattern = { "yorumi" },
+				callback = function(args)
+					apply_theme_overrides("yorumi", vim.o.background)
 				end,
 			})
 		end,
