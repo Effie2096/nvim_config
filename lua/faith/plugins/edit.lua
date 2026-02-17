039a0c9913b6bbb7b6cf92e2baeb6d29ee0cadd1
effie <havealittlefaith2096@gmail.com>
2026-02-17 21:53:46 +0000

    fix(keymap): dial keymaps

--- lua/faith/plugins/edit.lua
+++ lua/faith/plugins/edit.lua
@@ -91,17 +91,65 @@ return {
 	{
 		"monaqa/dial.nvim",
 		keys = {
-			-- stylua: ignore start
-			{ "<C-a>",function() require("dial.map").manipulate("increment", "normal") end, {"n"} },
-			{ "<C-x>",function() require("dial.map").manipulate("decrement", "normal") end, {"n"} },
-			{ "g<C-a>",function() require("dial.map").manipulate("increment", "gnormal") end, {"n"} },
-			{ "g<C-x>",function() require("dial.map").manipulate("decrement", "gnormal") end, {"n"} },
-			{ "<C-a>",function() require("dial.map").manipulate("increment", "visual") end, {"x"} },
-			{ "<C-x>",function() require("dial.map").manipulate("decrement", "visual") end, {"x"} },
-			{ "g<C-a>",function() require("dial.map").manipulate("increment", "gvisual") end, {"x"} },
-			{ "g<C-x>",function() require("dial.map").manipulate("decrement", "gvisual") end, {"x"} },
-			-- stylua: ignore end
+			{ "<C-a>" },
+			{ "<C-x>" },
+			{ "g<C-a>" },
+			{ "g<C-x>" },
+			{ "<C-a>" },
+			{ "<C-x>" },
+			{ "g<C-a>" },
+			{ "g<C-x>" },
 		},
+		config = function()
+			local augend = require("dial.augend")
+			require("dial.config").augends:register_group({
+				default = {
+					augend.integer.alias.decimal,
+					augend.integer.alias.hex,
+					augend.date.alias["%Y/%m/%d"],
+					augend.constant.new({
+						elements = { "and", "or" },
+						word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+						cyclic = true, -- "or" is incremented into "and".
+					}),
+					augend.constant.new({
+						elements = { "&&", "||" },
+						word = false,
+						cyclic = true,
+					}),
+					augend.hexcolor.new({
+						case = "prefer_upper", -- or "lower", "prefer_upper", "prefer_lower", see below
+					}),
+					augend.semver.alias.semver,
+					augend.constant.alias.bool,
+					augend.constant.alias.Bool,
+				},
+			})
+			vim.keymap.set("n", "<C-a>", function()
+				require("dial.map").manipulate("increment", "normal")
+			end)
+			vim.keymap.set("n", "<C-x>", function()
+				require("dial.map").manipulate("decrement", "normal")
+			end)
+			vim.keymap.set("n", "g<C-a>", function()
+				require("dial.map").manipulate("increment", "gnormal")
+			end)
+			vim.keymap.set("n", "g<C-x>", function()
+				require("dial.map").manipulate("decrement", "gnormal")
+			end)
+			vim.keymap.set("x", "<C-a>", function()
+				require("dial.map").manipulate("increment", "visual")
+			end)
+			vim.keymap.set("x", "<C-x>", function()
+				require("dial.map").manipulate("decrement", "visual")
+			end)
+			vim.keymap.set("x", "g<C-a>", function()
+				require("dial.map").manipulate("increment", "gvisual")
+			end)
+			vim.keymap.set("x", "g<C-x>", function()
+				require("dial.map").manipulate("decrement", "gvisual")
+			end)
+		end,
 	},
 	"tpope/vim-repeat",
 	{
