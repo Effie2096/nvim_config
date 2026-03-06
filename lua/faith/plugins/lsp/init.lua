4b4033b57350e7084dab9fc7db501f21c90abb18
effie <havealittlefaith2096@gmail.com>
2026-03-06 14:46:29 +0000

    feat(spell): remove dictionary suggestions from commit buffers

--- lua/faith/plugins/lsp/init.lua
+++ lua/faith/plugins/lsp/init.lua
@@ -623,10 +623,7 @@ return {
 					}
 					if
 						-- turn on dictionary in markdown or text file
-						vim.tbl_contains(
-							{ "markdown", "text", "gitcommit" },
-							vim.bo.filetype
-						)
+						vim.tbl_contains({ "markdown", "text" }, vim.bo.filetype)
 						-- or turn on dictionary if cursor is in the comment block
 						or inside_comment_block()
 					then
