a2f4842350fa0b59facb912a8ed3324af28c330f
Faith <havealittlefaith2096@gmail.com>
2026-06-05 19:04:19 +0100

    fix(plugins/lint): only run when linter available

--- lua/faith/plugins/lint.lua
+++ lua/faith/plugins/lint.lua
@@ -42,8 +42,12 @@ local config = function()
 			-- Only run the linter in buffers that you can modify in order to
 			-- avoid superfluous noise, notably within the handy LSP pop-ups that
 			-- describe the hovered symbol using Markdown.
+
 			if vim.bo.modifiable then
-				lint.try_lint()
+				local linter = require("lint").linters_by_ft[vim.bo.filetype]
+				if linter and vim.fn.executable(linter[1]) == 1 then
+					lint.try_lint()
+				end
 			end
 		end,
 	})
